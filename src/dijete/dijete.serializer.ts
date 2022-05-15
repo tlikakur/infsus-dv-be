846c74f3b5425f165d866d72356c73165c79c1be
Tomislav Likakur <tomislav.likakur@bebabit.com>
2022-05-15 21:12:51 +0200

    fix: dijeteserializer identifier

--- src/dijete/dijete.serializer.ts
+++ src/dijete/dijete.serializer.ts
@@ -1,6 +1,6 @@
 import { Serializer } from 'jsonapi-serializer';
 
-export const DijeteSerializer = new Serializer('children', {
+export const DijeteSerializer = new Serializer('child', {
   keyForAttribute: 'camelCase',
   id: 'iddijete',
   attributes: ['oib', 'ime', 'prezime' , 'datumrodenja']
