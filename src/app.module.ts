a8836e64d5feac7c6d391e6c8c456d3fdb9166fa
Tomislav Likakur <tomislav.likakur@bebabit.com>
2022-05-15 15:46:44 +0200

    fix: project settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
+import { dbConfig } from './config/config.service';
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GrupaModule } from './grupa/grupa.module';
 import { DijeteModule } from './dijete/dijete.module';
 import { BolestModule } from './bolest/bolest.module';
 
+
 @Module({
-  imports: [GrupaModule, DijeteModule, BolestModule],
+  imports: [
+    TypeOrmModule.forRoot(dbConfig.getTypeOrmConfig()), 
+    GrupaModule, 
+    DijeteModule, 
+    BolestModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule { }
+
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { config } from 'dotenv';
-config();
+import { appConfig } from './config/config.service'
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  await app.listen(appConfig.port);
 }
+
 bootstrap();
